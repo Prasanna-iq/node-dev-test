ed0f25edadf4607b3e388bcc169b3ecca93644b5
Prasanna Venkatesh <Prasanna-iq>
2023-07-11 15:17:28 +0530

    reverted amazon linux

--- devOps/scripts/install_dependencies.sh
+++ devOps/scripts/install_dependencies.sh
@@ -2,8 +2,7 @@
 mkdir /root/nodejs-service
 
 sudo chmod -R 777 /root/
-sudo apt update  
+sudo yum update  
 curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo npm install pm2 -g 
+sudo yum install nodejs -y
+sudo npm install pm2 -g
